6f3d2d60789fc62f7697b7230fb589f27ed0ff53
walkerwk <walkerwk@febdd8de-34b2-09fa-199c-44fb402a6750>
2011-12-18 14:27:53 +0000

    self define viewgroup and freebutton

--- Android/JavaCodeView/src/com/remotec/Test/FreeLayout.java
+++ Android/JavaCodeView/src/com/remotec/Test/FreeLayout.java
@@ -4,17 +4,23 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.View;
+import android.view.ViewGroup;
 
-public class FreeButton extends View {
-    Paint paintText;
+public class FreeLayout extends ViewGroup {
+	Paint paintText;
 	
-	public FreeButton(Context context) {
+	public FreeLayout(Context context) {
 		super(context);
-		paintText=new Paint();
-		paintText.setColor(Color.GREEN);
 		this.setWillNotDraw(false);
 		// TODO Auto-generated constructor stub
+		paintText = new Paint();
+		paintText.setColor(Color.GREEN);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+
 	}
 	
     /**
@@ -23,10 +29,14 @@ public class FreeButton extends View {
      * @param canvas the canvas on which the background will be drawn
      */
     protected void onDraw(Canvas canvas) {
+    	super.onDraw(canvas);
     	
-    	canvas.drawColor(Color.BLUE);
+    	this.dispatchDraw(canvas);
+    
+//    	canvas.drawColor(Color.WHITE);
 //    	canvas.clipRect(120, 100, 240, 280);
-    	canvas.drawText("I'm a button", 120, 110, paintText);
+//    	canvas.drawText("I'm FreeLayout", 10, 10, paintText);
+    	
     }
 
 }
--- Android/JavaCodeView/src/com/remotec/Test/JavaCodeView.java
+++ Android/JavaCodeView/src/com/remotec/Test/JavaCodeView.java
@@ -12,15 +12,24 @@ public class JavaCodeView extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        
-        LinearLayout lo = new LinearLayout(this); //宽度为100px，高为自适应最小的高度
-        setContentView(lo);
-        // setOrientation(VERTICAL); 设置布局为垂直
-
-        TextView textControl = new TextView(this);//如果从一个XXXLayout.，比如LinearLayout为View的基类时这里this应该换成为创建改类的Context 
-        textControl.setText("Android开发网欢迎您"); 
-    
+//        LinearLayout lo = new LinearLayout(this); //宽度为100px，高为自适应最小的高度
+//        setContentView(lo);
+//        // setOrientation(VERTICAL); 设置布局为垂直
+//
+//        TextView textControl = new TextView(this);//如果从一个XXXLayout.，比如LinearLayout为View的基类时这里this应该换成为创建改类的Context 
+//        textControl.setText("Android开发网欢迎您"); 
+//    
+//        FreeButton fb=new FreeButton(this);
+//        fb.setWillNotDraw(false);
+//        addContentView(fb, new LinearLayout.LayoutParams(1000, 1000) );
+        
+        FreeLayout fl=new FreeLayout(this);
+        setContentView(fl);
+        
         FreeButton fb=new FreeButton(this);
-        addContentView(fb, new LinearLayout.LayoutParams(1000, 1000) );
+        fl.addView(fb);
+        
+//        addContentView(fb);
 
 
     }
