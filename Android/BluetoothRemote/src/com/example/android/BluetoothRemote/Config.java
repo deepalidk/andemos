ea7d2d71fc409428b75e5b77f518a913ca465f65
walkerwk <walkerwk@febdd8de-34b2-09fa-199c-44fb402a6750>
2012-02-17 09:33:30 +0000

    1.filter version for 1232x800 screen. bugs: 1.not prefix version. 2.total refresh bug. 3. not support other resolution.

---
+++ Android/BluetoothRemote/src/com/example/android/BluetoothRemote/Config.java
@@ -0,0 +1,12 @@
+package com.example.android.BluetoothRemote;
+
+public class Config {
+    //获取SDcard路径
+    public static String CR_SDCARD = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
+    
+    //定义SDcard存放数据的文件夹
+    public static final String CR_DRECTORY= "/remotec";
+    public static final String CR_PATH = CR_SDCARD+CR_DRECTORY;
+    public static final String CR_DBNAME = "remote.db";
+    public static final String CR_DBPATH = CR_PATH+"/"+CR_DBNAME;//database file
+}
---
+++ Android/BluetoothRemote/src/com/example/android/BluetoothRemote/DbManager.java
@@ -0,0 +1,35 @@
+package com.example.android.BluetoothRemote;
+
+import java.io.File;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class DbManager {	
+	
+	private static SQLiteDatabase mDataBase=null;
+	
+	public static SQLiteDatabase Handle()
+	{
+		if(mDataBase==null)
+		{
+			mDataBase=openDatabase();
+		}
+		
+		return mDataBase;
+	}
+	
+	private DbManager(){}
+	
+	private static SQLiteDatabase openDatabase() { 
+        try { 
+            // 获得dictionary.db文件的绝对路径 
+            File dbf = new File(Config.CR_DBPATH); 
+            // 打开/sdcard/dictionary目录中的dictionary.db文件 
+            SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase( 
+            		dbf, null); 
+            return database; 
+        } catch (Exception e) { 
+        } 
+        return null; 
+    } 
+}
