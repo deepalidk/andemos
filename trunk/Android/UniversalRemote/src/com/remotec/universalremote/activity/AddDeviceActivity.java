4a9e5b6fcf10db4a2b279c264a6504c73c2611e0
walkerwk <walkerwk@febdd8de-34b2-09fa-199c-44fb402a6750>
2012-03-30 07:52:41 +0000

    add select icon

--- trunk/Android/UniversalRemote/src/com/remotec/universalremote/activity/AddDeviceActivity.java
+++ trunk/Android/UniversalRemote/src/com/remotec/universalremote/activity/AddDeviceActivity.java
@@ -5,7 +5,6 @@
  */
 package com.remotec.universalremote.activity;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,198 +39,193 @@ import android.widget.Spinner;
  *Displays device for UI.
  */
 public class AddDeviceActivity extends Activity {
-	
+
 	private static final int REQUEST_SELECT_ICON = 1;
 	// Debugging Tags
 	private static final String TAG = "AddDeviceActivity";
 	private static final boolean D = false;
-	 
-	private List<DeviceButton> mDevButtonList=null;
-	
-    private Spinner mSpinerCategory;    
-    private Spinner mSpinerManufacturer;   
-    private Spinner mSpinerModel;   
-    
-    private ArrayAdapter adapter2;  
-    
-    private ViewGroup mVgSelectDevice;
-    private ViewGroup mVgDeviceInfo;
-    
-    private Button mBtnCancel;
-    private Button mBtnNext;
-    private Button mBtnBack;
-    private Button mBtnFinish;
-    private Button mBtnTest;
-    
-    private EditText mDeviceName;   //name edittext
-    
-    private ImageView mDeviceIcon;
-	
-    enum eCurrentPage
-    {
-    	eSelectDevice,
-    	eDeviceInfo
-    };
-    
-    private eCurrentPage mCurPage;
-    
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        //remove the tile.
+
+	private List<DeviceButton> mDevButtonList = null;
+
+	private Spinner mSpinerCategory;
+	private Spinner mSpinerManufacturer;
+	private Spinner mSpinerModel;
+
+	private ArrayAdapter adapter2;
+
+	private ViewGroup mVgSelectDevice;
+	private ViewGroup mVgDeviceInfo;
+
+	private Button mBtnCancel;
+	private Button mBtnNext;
+	private Button mBtnBack;
+	private Button mBtnFinish;
+	private Button mBtnTest;
+
+	private EditText mDeviceName; // name edittext
+
+	private ImageView mDeviceIcon;
+
+	enum eCurrentPage {
+		eSelectDevice, eDeviceInfo
+	};
+
+	private eCurrentPage mCurPage;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// remove the tile.
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.add_device_wizard);
-        
+		setContentView(R.layout.add_device_wizard);
+
 		getWindow().setSoftInputMode(
 				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        
-	    setResult(RESULT_CANCELED);
-		
-        initControls();
-        
-        mCurPage=eCurrentPage.eSelectDevice;
-      
-		//添加事件Spinner事件监听          
-		//spinner2.setOnItemSelectedListener(new SpinnerXMLSelectedListener());       
-    }
-    
-    void initCurrentPage(eCurrentPage curPage)
-    {
-		mCurPage=curPage;
-    	if(curPage==eCurrentPage.eSelectDevice)
-    	{
-    	       mBtnNext.setVisibility(View.VISIBLE);
-    	       mBtnFinish.setVisibility(View.INVISIBLE);
-    	       mBtnBack.setVisibility(View.INVISIBLE);
-    	       mVgSelectDevice.setVisibility(View.VISIBLE);
-    	       mVgDeviceInfo.setVisibility(View.INVISIBLE);
-    	}
-    	else if(curPage==eCurrentPage.eDeviceInfo)
-    	{
-    
- 	       mBtnNext.setVisibility(View.INVISIBLE);
-	       mBtnFinish.setVisibility(View.VISIBLE);
-	       mBtnBack.setVisibility(View.VISIBLE);
-	       mVgSelectDevice.setVisibility(View.INVISIBLE);
-	       mVgDeviceInfo.setVisibility(View.VISIBLE);
-	       
-	       InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE); 
-	       imm.hideSoftInputFromWindow(mDeviceName.getWindowToken(),0);
-    	}
-    }
+
+		setResult(RESULT_CANCELED);
+
+		initControls();
+
+		mCurPage = eCurrentPage.eSelectDevice;
+
+		// 添加事件Spinner事件监听
+		// spinner2.setOnItemSelectedListener(new SpinnerXMLSelectedListener());
+	}
+
+	void initCurrentPage(eCurrentPage curPage) {
+		mCurPage = curPage;
+		if (curPage == eCurrentPage.eSelectDevice) {
+			mBtnNext.setVisibility(View.VISIBLE);
+			mBtnFinish.setVisibility(View.INVISIBLE);
+			mBtnBack.setVisibility(View.INVISIBLE);
+			mVgSelectDevice.setVisibility(View.VISIBLE);
+			mVgDeviceInfo.setVisibility(View.INVISIBLE);
+		} else if (curPage == eCurrentPage.eDeviceInfo) {
+
+			mBtnNext.setVisibility(View.INVISIBLE);
+			mBtnFinish.setVisibility(View.VISIBLE);
+			mBtnBack.setVisibility(View.VISIBLE);
+			mVgSelectDevice.setVisibility(View.INVISIBLE);
+			mVgDeviceInfo.setVisibility(View.VISIBLE);
+
+			InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(mDeviceName.getWindowToken(), 0);
+		}
+	}
 
 	private void initControls() {
-		
-		mSpinerCategory = (Spinner) findViewById(R.id.spiner_category);     
-        mSpinerManufacturer = (Spinner) findViewById(R.id.spiner_manufacturer);   
-        mSpinerModel = (Spinner) findViewById(R.id.spiner_model);   
-        
-        mVgSelectDevice=(ViewGroup)findViewById(R.id.select_ircode);
-        mVgDeviceInfo=(ViewGroup)findViewById(R.id.device_info);
-        
-        mBtnCancel=(Button)findViewById(R.id.btn_footer_cancel);
-        mBtnCancel.setOnClickListener(mOnCancelListener);
-        
-        mBtnNext=(Button)findViewById(R.id.btn_footer_next);
-        mBtnNext.setOnClickListener(mOnNextListener);
-        
-        mBtnFinish=(Button)findViewById(R.id.btn_footer_finish);
-        mBtnFinish.setOnClickListener(mOnFinishListener);
-        
-        mBtnBack=(Button)findViewById(R.id.btn_footer_back);
-        mBtnBack.setOnClickListener(mOnBackListener);
-        
-        mBtnTest=(Button)findViewById(R.id.btn_test);
-        mBtnTest.setOnClickListener(mOnTestListener);
-        
-        mDeviceName=(EditText)findViewById(R.id.name_edit);
-        
-        mDeviceIcon=(ImageView)findViewById(R.id.device_img);
-        mDeviceIcon.setOnClickListener(mOnIconListener);
-      
-        //将可选内容与ArrayAdapter连接起来        
-        adapter2 = ArrayAdapter.createFromResource(this, R.array.dev_category, R.layout.irremote_spinner);       
-      
-        //设置下拉列表的风格         
-        adapter2.setDropDownViewResource(R.layout.irremote_spinner_item);       
-      
-        //将adapter2 添加到spinner中        
-        mSpinerCategory.setAdapter(adapter2);   
-        mSpinerManufacturer.setAdapter(adapter2);
-        mSpinerModel.setAdapter(adapter2); 
+
+		mSpinerCategory = (Spinner) findViewById(R.id.spiner_category);
+		mSpinerManufacturer = (Spinner) findViewById(R.id.spiner_manufacturer);
+		mSpinerModel = (Spinner) findViewById(R.id.spiner_model);
+
+		mVgSelectDevice = (ViewGroup) findViewById(R.id.select_ircode);
+		mVgDeviceInfo = (ViewGroup) findViewById(R.id.device_info);
+
+		mBtnCancel = (Button) findViewById(R.id.btn_footer_cancel);
+		mBtnCancel.setOnClickListener(mOnCancelListener);
+
+		mBtnNext = (Button) findViewById(R.id.btn_footer_next);
+		mBtnNext.setOnClickListener(mOnNextListener);
+
+		mBtnFinish = (Button) findViewById(R.id.btn_footer_finish);
+		mBtnFinish.setOnClickListener(mOnFinishListener);
+
+		mBtnBack = (Button) findViewById(R.id.btn_footer_back);
+		mBtnBack.setOnClickListener(mOnBackListener);
+
+		mBtnTest = (Button) findViewById(R.id.btn_test);
+		mBtnTest.setOnClickListener(mOnTestListener);
+
+		mDeviceName = (EditText) findViewById(R.id.name_edit);
+
+		mDeviceIcon = (ImageView) findViewById(R.id.device_img);
+		mDeviceIcon.setOnClickListener(mOnIconListener);
+
+		// 将可选内容与ArrayAdapter连接起来
+		adapter2 = ArrayAdapter.createFromResource(this, R.array.dev_category,
+				R.layout.irremote_spinner);
+
+		// 设置下拉列表的风格
+		adapter2.setDropDownViewResource(R.layout.irremote_spinner_item);
+
+		// 将adapter2 添加到spinner中
+		mSpinerCategory.setAdapter(adapter2);
+		mSpinerManufacturer.setAdapter(adapter2);
+		mSpinerModel.setAdapter(adapter2);
 	}
-	
+
 	/*
 	 * Deals cancel button click.
 	 */
-   private OnClickListener mOnCancelListener=new OnClickListener(){
+	private OnClickListener mOnCancelListener = new OnClickListener() {
 
-	@Override
-	public void onClick(View v) {
-		AddDeviceActivity.this.setResult(RESULT_CANCELED);
-		AddDeviceActivity.this.finish();
-	}
-	   
-   };
-   
+		@Override
+		public void onClick(View v) {
+			AddDeviceActivity.this.setResult(RESULT_CANCELED);
+			AddDeviceActivity.this.finish();
+		}
+
+	};
+
 	/*
 	 * Deals back button click.
 	 */
-  private OnClickListener mOnBackListener=new OnClickListener(){
+	private OnClickListener mOnBackListener = new OnClickListener() {
 
-	@Override
-	public void onClick(View v) {
-		AddDeviceActivity.this.initCurrentPage(eCurrentPage.eSelectDevice);
-	}
-  };
-  
+		@Override
+		public void onClick(View v) {
+			AddDeviceActivity.this.initCurrentPage(eCurrentPage.eSelectDevice);
+		}
+	};
+
 	/*
 	 * Deals next button click.
 	 */
-private OnClickListener mOnNextListener=new OnClickListener(){
+	private OnClickListener mOnNextListener = new OnClickListener() {
 
-	@Override
-	public void onClick(View v) {
-		AddDeviceActivity.this.initCurrentPage(eCurrentPage.eDeviceInfo);
-	}
-};
+		@Override
+		public void onClick(View v) {
+			AddDeviceActivity.this.initCurrentPage(eCurrentPage.eDeviceInfo);
+		}
+	};
 
-/*
- * Deals finish button click.
- */
-private OnClickListener mOnFinishListener=new OnClickListener(){
+	/*
+	 * Deals finish button click.
+	 */
+	private OnClickListener mOnFinishListener = new OnClickListener() {
 
-@Override
-public void onClick(View v) {
-	AddDeviceActivity.this.setResult(RESULT_OK);
-	AddDeviceActivity.this.finish();
-}
-};
+		@Override
+		public void onClick(View v) {
+			AddDeviceActivity.this.setResult(RESULT_OK);
+			AddDeviceActivity.this.finish();
+		}
+	};
 
-/*
- * Deals finish button click.
- */
-private OnClickListener mOnTestListener=new OnClickListener(){
+	/*
+	 * Deals finish button click.
+	 */
+	private OnClickListener mOnTestListener = new OnClickListener() {
 
-@Override
-public void onClick(View v) {
+		@Override
+		public void onClick(View v) {
 
-}
-};
+		}
+	};
 
-/*
- * Deals finish button click.
- */
-private OnClickListener mOnIconListener=new OnClickListener(){
+	/*
+	 * Deals finish button click.
+	 */
+	private OnClickListener mOnIconListener = new OnClickListener() {
 
-@Override
-public void onClick(View v) {
-	Intent addDeviceIntent = new Intent(AddDeviceActivity.this, SelectIconDialog.class);
-	startActivityForResult(addDeviceIntent, REQUEST_SELECT_ICON);
-}
-};
-    
-    
+		@Override
+		public void onClick(View v) {
+			Intent addDeviceIntent = new Intent(AddDeviceActivity.this,
+					SelectIconDialog.class);
+			startActivityForResult(addDeviceIntent, REQUEST_SELECT_ICON);
+		}
+	};
+
 }
--- trunk/Android/UniversalRemote/src/com/remotec/universalremote/activity/component/ImageAdapter.java
+++ trunk/Android/UniversalRemote/src/com/remotec/universalremote/activity/component/ImageAdapter.java
@@ -5,6 +5,10 @@
  */
 package com.remotec.universalremote.activity.component;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
@@ -17,60 +21,69 @@ import android.widget.ImageView;
 import com.remotec.universalremote.activity.R;
 import com.remotec.universalremote.data.Device;
 
+public class ImageAdapter extends BaseAdapter {
 
-public class ImageAdapter extends BaseAdapter  {
-	
 	private Context mContext;
-	 
-	 public ImageAdapter(Context c)
-	 {
-	  mContext=c;
-	 }
-	 @Override
-	 public int getCount() {
-	  // TODO Auto-generated method stub
-	  return mThumbIds.length;
-	 }
 
-	 @Override
-	 public Object getItem(int position) {
-	  // TODO Auto-generated method stub
-	  return null;
-	 }
+	public ImageAdapter(Context c) {
+		mContext = c;
+	}
 
-	 @Override
-	 public long getItemId(int position) {
-	  // TODO Auto-generated method stub
-	  return 0;
-	 }
+	@Override
+	public int getCount() {
+		return mThumbIds.size();
+	}
 
-	 
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
 
-	 @Override
-	 public View getView(int position, View convertView, ViewGroup parent) {
-	  // TODO Auto-generated method stub
-	  
-	  ImageView imageview;
-	  if(convertView==null)
-	  {
-	   imageview=new ImageView(mContext);
-	   imageview.setLayoutParams(new GridView.LayoutParams(85, 85));
-	   imageview.setScaleType(ImageView.ScaleType.CENTER_CROP);
-	   imageview.setPadding(8,8,8,8);
-	 }
-	  else
-	  {
-	   imageview=(ImageView) convertView;
-	  }
-	  imageview.setImageResource(mThumbIds[position]);
-	  return imageview;
-	  }
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
 
-	 private Integer[] mThumbIds={//显示的图片数组
-	  
-	  R.drawable.icon_device_doc2_s
-	 };
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imageview;
+		if (convertView == null) {
+			imageview = new ImageView(mContext);
+			imageview.setLayoutParams(new GridView.LayoutParams(85, 85));
+			imageview.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageview.setPadding(8, 8, 8, 8);
+		} else {
+			imageview = (ImageView) convertView;
+		}
+		imageview.setImageResource(mThumbIds.get(position));
+		return imageview;
+	}
 
+	// 获取连连看所有图片的ID（约定所有图片ID以p_开头）
+	public static List<Integer> getImageValues()
+	{
+		try
+		{
+			//得到R.drawable所有的属性, 即获取drawable目录下的所有图片
+			Field[] drawableFields = R.drawable.class.getFields();
 
+			List<Integer> resourceValues = new ArrayList();
+
+			for (Field field : drawableFields){
+				// 如果该Field的名称以p_开头
+				if ((field.getName().indexOf("icon_") != -1)&&(field.getName().indexOf("_s") != -1)){
+					resourceValues.add(field.getInt(R.drawable.class));
+				}
+			}
+
+			return resourceValues;
+
+		}catch (Exception e){
+			return null;
+		}
+
+	}
+
+	private  static List<Integer> mThumbIds = getImageValues();
 
 }
