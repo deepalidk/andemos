c204ff5a79faed5a355066dc271d3765d405edf9
walkerwk <walkerwk@febdd8de-34b2-09fa-199c-44fb402a6750>
2012-09-28 08:19:45 +0000

    1. v1.7

--- trunk/Android/UniversalRemote/src/com/remotec/universalremote/data/RemoteUi.java
+++ trunk/Android/UniversalRemote/src/com/remotec/universalremote/data/RemoteUi.java
@@ -98,7 +98,7 @@ public class RemoteUi {
         */
        public static int communicationMode()
        {
-    	   return SERIAL_MODE;
+    	   return BT_MODE;
        }
        
        private String mVersion;
